00e4cc48c0a514e6e1f2cf33b225ec3966877e68
Uwe Riegel <uriegel@hotmail.de>
2022-11-17 09:23:43 +0100

    Tester

--- Tester/Tests.fs
+++ Tester/Tests.fs
@@ -13,19 +13,19 @@ type ClassDataBase(generator : obj [] seq) =
 
 type getStringUrls () = 
     inherit ClassDataBase([ 
-        // [| null; 34 |]        
+        [| null; 34 |]        
         // [| ""; 34 |]        
         // [| "dump"; 34 |]        
         [| "http://nodomain.no"; 34 |]        
       //  [| "https://caesar2go.caseris.de:999"; 34 |]        
         [| "https://caesar2go.caseris.de/nopath"; 34 |]        
-        [| "http://checkip.dyndns.org"; 3456 |] 
+        [| "http://checkip.dyndns.org"; 34 |] 
     ])
 
 type Tests(output: ITestOutputHelper) =
     [<Theory>]
     [<ClassData(typeof<getStringUrls>)>]
-    let ``let getString (url, result)`` (url, result) = 
+    let getString (url, result) = 
         output.WriteLine (sprintf "\n=================================================================\ngetString - %s\n" url)
         async {
             let settings = { Request.defaultSettings with Url = url }
@@ -33,9 +33,7 @@ type Tests(output: ITestOutputHelper) =
             match! Request.getString settings with
             | Ok ok  -> output.WriteLine (sprintf "OK:\n%s" ok)
             | Error err -> 
-                let affe = exn
                 output.WriteLine (sprintf "Err:\n%s" err.Message)
-                ()
             Assert.Equal(34, result)
         } 
         |> Async.RunSynchronously   
